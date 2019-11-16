b0b8c57b404a26eb11c42024864514dfcb5c3112
Alice <alicehzheng@gmail.com>
2019-11-16 13:41:32 -0500

    Updae create_index_map by adding text-entity mapping

--- scripts/create_index_map.py
+++ scripts/create_index_map.py
@@ -1,5 +1,5 @@
 """
-	Functionality: Take raw data as input and generate json dumps of entity-text mapping as well as entity-docid mapping
+	Functionality: Take raw data as input and generate json dumps of entity-text mapping, text-entity mapping as well as entity-docid mapping
 """
 
 import argparse
@@ -9,6 +9,7 @@ def get_args():
 	parser = argparse.ArgumentParser(description='Build Index Map')
 	parser.add_argument('--input-path', type=str, help='json dump file as input')
 	parser.add_argument('--entity2text-path', type=str, help='path to store entity-text mapping')
+	parser.add_argument('--text2entity-path', type=str, help='path to store text-entity mapping')
 	parser.add_argument('--entity2doc-path', type=str, help='path to store entity-docid mapping')
 
 	return parser.parse_args()
@@ -16,6 +17,7 @@ def get_args():
 def main():
 	args = get_args()
 	entity_text_map = {} # key:entity, value: a list of corresponding text(s)
+	text_entity_map = {} # key:text, value: corresponding entity
 	entity_doc_map = {} # key:entity, value: a list of document ids that the entity occurs in 
 
 
@@ -32,6 +34,11 @@ def main():
 					elif text not in entity_text_map[entity]:
 							entity_text_map[entity].append(text)
 					"""
+						update text_entity_map
+					"""
+					text_entity_map[text] = entity
+
+					"""
 						update entity_doc_map
 					"""
 					if entity not in entity_doc_map:
@@ -40,6 +47,8 @@ def main():
 						entity_doc_map[entity].append(doc_id)
 	with open(args.entity2text_path, "w", encoding = "utf-8") as output_file:
 		json.dump(entity_text_map, output_file)
+	with open(args.text2entity_path, "w", encoding = "utf-8") as output_file:
+		json.dump(text_entity_map, output_file)
 	with open(args.entity2doc_path, "w", encoding = "utf-8") as output_file:
 		json.dump(entity_doc_map, output_file)
 
